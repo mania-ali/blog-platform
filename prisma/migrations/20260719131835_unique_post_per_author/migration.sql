c36ab58fcb0d6be349a4d3dcc458ef3424119a67
Mania <mania.ali557@gmail.com>
2026-07-22 05:50:32 -0700

    Remove duplicate backend files from old root path, add frontend assets

--- prisma/migrations/20260719131835_unique_post_per_author/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title,authorId]` on the table `Post` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Post_title_authorId_key" ON "Post"("title", "authorId");
