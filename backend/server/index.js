9f6c57d3d65d8af986c6bbce5c18fc04a5088bf6
Mania <mania.ali557@gmail.com>
2026-07-24 11:50:01 -0700

    Update app.listen host to 0.0.0.0 for Railway

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,25 +1,29 @@
 require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
 const authRoutes = require("./routes/auth");
-const express=require("express") //import express
-const app=express()  //create server empty
-const cors = require("cors");  //backend frontend communication
+const express = require("express"); // import express
+const app = express(); // create server empty
+const cors = require("cors"); // backend frontend communication
 const errorHandler = require("./middleware/errorMiddleware");
-const postRoutes=require("./routes/post");
+const postRoutes = require("./routes/post");
 const userRoutes = require("./routes/user");
 
 app.use(cors());
 app.use(express.json());
-const PORT=5000 
-//initial start of backend
-app.get("/",(req,res)=>{
-    res.send("backend is running")
+
+const PORT = process.env.PORT || 5000;
+
+// Initial start of backend
+app.get("/", (req, res) => {
+  res.send("backend is running");
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/posts",postRoutes);
+app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 
 app.use(errorHandler);
-app.listen(PORT,()=>{
-console.log(`Server running on port ${PORT}`);
+
+// Added '0.0.0.0' host parameter for Railway network routing
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
